62c7492fecc987d1f5ad1a1e089158900618bb42
konezero-admin <konezero@nate.com>
2016-12-16 15:31:24 +0900

    y

---
+++ test/test/include/ClassA.h
@@ -0,0 +1,20 @@
+//
+//  ClassA.h
+//  test
+//
+//  Created by IL YEONG KIM on 2016. 12. 16..
+//  Copyright © 2016년 IL YEONG KIM. All rights reserved.
+//
+
+#ifndef ClassA_h
+#define ClassA_h
+#import <Foundation/NSObject.h>
+
+static int count;
+
+@interface ClassA: NSObject
++(int) initCount;
++(void) initialize;
+@end
+
+#endif /* ClassA_h */
---
+++ test/test/include/T_Exception.h
@@ -0,0 +1,14 @@
+//
+//  T_Exception.h
+//  test
+//
+//  Created by IL YEONG KIM on 2016. 12. 16..
+//  Copyright © 2016년 IL YEONG KIM. All rights reserved.
+//
+
+#ifndef T_Exception_h
+#define T_Exception_h
+#import <Foundataion/NSException.h>
+
+
+#endif /* T_Exception_h */
