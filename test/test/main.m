8128c22747b4601ef6c18173423146a5145e7b4d
konezero-admin <konezero@nate.com>
2016-12-16 15:28:44 +0900

    t

--- test/test/main.m
+++ test/test/main.m
@@ -6,12 +6,29 @@
 //  Copyright © 2016년 IL YEONG KIM. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <stdio.h>
+#import "./include/ClassA.h"
 
 int main(int argc, const char * argv[]) {
+    /*
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
     }
+     */
+    
+    ClassA *c1 = [[ClassA alloc] init];
+    ClassA *c2 = [[ClassA alloc] init];
+    
+    //print count
+    printf( "ClassA count: %i\n", [ClassA initCount]);
+    
+    ClassA *c3 = [[ClassA alloc] init];
+
+    //print count again
+    printf( "ClassA count: %i\n", [ClassA initCount]);
+    
+    [c1 release];
+    [c2 release];
+    [c3 release];
     return 0;
 }
