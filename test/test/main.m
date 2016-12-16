0f968f79d9b81a0b48e63efb86050d86b3145e17
konezero-admin <konezero@nate.com>
2016-12-16 15:15:31 +0900

    test create

---
+++ test/test/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  test
+//
+//  Created by IL YEONG KIM on 2016. 12. 16..
+//  Copyright © 2016년 IL YEONG KIM. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
